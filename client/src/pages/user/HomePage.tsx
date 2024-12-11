eff8ad02953c159d2a149c5cc425f9a6c11d02c9
Yanuar Eka <pramudyanuar@gmail.com>
2024-12-12 01:51:50 +0700

    fix homepage

--- client/src/pages/user/HomePage.tsx
+++ client/src/pages/user/HomePage.tsx
@@ -4,6 +4,7 @@ import HeroSection from '../../components/HeroSection';
 import Footer from '../../components/Footer';
 import LogoSection from '../../components/LogoSection';
 import IconComponents from '../../components/FeatureList';
+import FullScreenVideoPlayer from '../../components/VideoSection';
 
 const HomePage: React.FC = () => {
   const [showDisclaimer, setShowDisclaimer] = useState(true);
@@ -22,6 +23,7 @@ const HomePage: React.FC = () => {
       <HeroSection />
       <div className='mt-3 mb-2'>
         <LogoSection />
+        <FullScreenVideoPlayer />
         <IconComponents />
       </div>
 
